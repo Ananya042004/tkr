f0c45eb65cd3325bb5ec6f17a82926e2a6581f75
Ananyach557 <ananyachinthireddy@gmail.com>
2025-09-17 10:27:08 +0530

    fourth commit

---
+++ Ananya/src/com/tnsif/first/palindrome.java
@@ -0,0 +1,25 @@
+package com.tnsif.first;
+
+public class palindrome {
+
+	public static void main(String[] args) {
+		int num=121;
+		int temp=num;
+		int rev=0;
+		while(temp>0)
+		{
+			int digit=temp%10;
+			rev=rev*10+digit;
+			temp=temp/10;
+		}
+		if(num==rev)
+		{
+			System.out.println(num + "is a palindrome");
+		}else
+		{
+			System.out.println(num + "is not a palindrome");
+		}
+
+	}
+
+}
