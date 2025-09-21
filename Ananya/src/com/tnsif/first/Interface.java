5b03bcab7fe4e1b0bf8c9bbcab8bee4876be9213
Ananyach557 <ananyachinthireddy@gmail.com>
2025-09-21 21:53:56 +0530

    commit interface

---
+++ Ananya/src/com/tnsif/first/Interface.java
@@ -0,0 +1,30 @@
+package com.tnsif.first;
+interface Animal{
+	void eat();
+}
+interface pet extends Animal
+{
+	void play();
+}
+class dog implements pet
+{
+	public void eat()
+	{
+		System.out.println("dog is eating");
+	}
+	public void play()
+	{
+		System.out.println("dog is playing");
+	}
+}
+public class Interface {
+
+	public static void main(String[] args) {
+		dog mydog=new dog();
+		mydog.eat();
+		mydog.play();
+		
+
+	}
+
+}
