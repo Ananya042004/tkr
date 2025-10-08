4aa8c8fb3d1736aa4cc96a9a45a1289222692912
Ananyach557 <ananyachinthireddy@gmail.com>
2025-10-08 23:29:02 +0530

    commit

---
+++ Ananya/src/com/tnsif/StreamApi/StreamApiDemo.java
@@ -0,0 +1,25 @@
+package com.tnsif.StreamApi;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamApiDemo {
+
+	public static void main(String[] args) {
+		List<Product> pl=new ArrayList<Product>();
+		
+		pl.add(new Product(1,"hp",25000f));
+		pl.add(new Product(2,"dell",30000f));
+		pl.add(new Product(1,"Apple",95000f));
+		pl.add(new Product(1,"lenova",15000f));
+		
+		List<Float>pl2=pl.stream().filter(p->p.price>30000)
+				.map(p->p.price)
+				.collect(Collectors.toList());
+		System.out.println(pl2);
+		
+
+	}
+
+}
