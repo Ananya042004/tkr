ee53509025ce51ac400a2c600b0f1b724797a9f2
Ananyach557 <ananyachinthireddy@gmail.com>
2025-10-08 19:24:51 +0530

    commit

---
+++ Ananya/src/com/tnsif/collections/GenericDemo.java
@@ -0,0 +1,18 @@
+package com.tnsif.collections;
+
+public class GenericDemo {
+
+	public static void main(String[] args) {
+		Generic<Integer>g=new Generic<Integer>();
+		g.setData(201);
+		g.setData(202);
+		System.out.println(g.getData());
+		
+		Generic<String>g1=new Generic<String>();
+		g1.setData("string");
+		System.out.println(g1.getData());
+			
+
+	}
+
+}
