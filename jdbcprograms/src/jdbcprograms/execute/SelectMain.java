7c519a55a2c0021cd93722d5c451c1e41cb19b28
Ananyach557 <ananyachinthireddy@gmail.com>
2025-10-10 13:48:03 +0530

    commit

--- jdbcprograms/src/jdbcprograms/execute/SelectMain.java
+++ jdbcprograms/src/jdbcprograms/execute/SelectMain.java
@@ -3,7 +3,7 @@ package jdbcprograms.execute;
 import jdbcprograms.services.Crudoperations;
 
 public class SelectMain {
-
+    //jdbc select
 	public static void main(String[] args) {
 		Crudoperations.displayAllStudents();
         Crudoperations.searchStudent(101);
